879d058bb600c43aac5a517784eeef175f2b2815
Dindon03 <dondelinger-jonathan@hotmail.fr>
2023-07-10 16:49:13 +0200

    10/07/2023

---
+++ add.js
@@ -0,0 +1,12 @@
+const overlapEls = document.querySelectorAll(".overlap") || [];
+overlapEls.forEach(el => {
+  const chars = [...el.textContent];
+  el.innerHTML = "";
+  chars.forEach((char,index) => {
+    const span = document.createElement("span");
+    span.textContent = char;
+    span.style.setProperty("--index", index)
+    el.append(span)
+  })
+})
+  

9f3ddbd8c3523d0ae3643573d5005fbcf9e7c0d9
Dindon03 <Dondelinger-jonathan@hotmail.fr>
2023-07-17 22:30:40 +0200

    17/07/2023

--- add.js
+++ add.js
@@ -1,12 +1,36 @@
-const overlapEls = document.querySelectorAll(".overlap") || [];
-overlapEls.forEach(el => {
-  const chars = [...el.textContent];
-  el.innerHTML = "";
-  chars.forEach((char,index) => {
-    const span = document.createElement("span");
-    span.textContent = char;
-    span.style.setProperty("--index", index)
-    el.append(span)
-  })
-})
-  
+const cube = document.querySelector('.cube');
+
+function rotateTo(face) {
+  cube.style.transform = `rotateX(${getRotationX(face)}) rotateY(${getRotationY(face)})`;
+}
+
+function getRotationX(face) {
+  switch (face) {
+    case 'front':
+      return '0deg';
+    case 'back':
+      return '180deg';
+    case 'left':
+    case 'right':
+      return '0deg';
+    case 'top':
+      return '-90deg';
+    case 'bottom':
+      return '90deg';
+  }
+}
+
+function getRotationY(face) {
+  switch (face) {
+    case 'front':
+    case 'back':
+      return '0deg';
+    case 'left':
+      return '-90deg';
+    case 'right':
+      return '90deg';
+    case 'top':
+    case 'bottom':
+      return '0deg';
+  }
+}
